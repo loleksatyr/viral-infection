07afdf3fbf17ccdf8533cd5603f081a3697afc46
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-03-22 22:02:55 +0100

    Find nearest customer

--- Assets/Scripts/CustomerController.cs
+++ Assets/Scripts/CustomerController.cs
@@ -8,22 +8,55 @@ public class CustomerController : MonoBehaviour
     public bool isActive = false;
     public GameObject nextIndicator;
     public GameObject currentIndicator;
+    public GameObject camera;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0f, 0f, 0f);
+
+    void Activate()
+    {
+        isActive = true;
+    }
+
+    IEnumerator SwitchCustomer(GameObject customer)
+    {
+        yield return new WaitForSeconds(1/20);
+        isActive = false;
+        customer.GetComponent<CustomerController>().isActive = true;
+    }
 
     void FixedUpdate()
     {
         if (isActive)
         {
             currentIndicator.transform.position = gameObject.transform.position + new Vector3(0, 2f, 0);
+            camera.transform.position = gameObject.transform.position + cameraOffset;
 
             nextIndicator.transform.position = new Vector3(-10000f, -10000f, -10000f);
             Collider[] customerColliders = Physics.OverlapSphere(gameObject.transform.position, sphereRadius);
+
+            GameObject nearestCustomer = null;
+            float nearestDistance = float.MaxValue;
+
             foreach (Collider collider in customerColliders)
             {
-                Debug.Log(collider.gameObject.tag);
-
                 if (collider.gameObject.tag == "Customer" && collider.gameObject.GetComponent<CustomerController>().isActive == false)
                 {
-                    nextIndicator.transform.position = collider.gameObject.transform.position + new Vector3(0, 2f, 0);
+                    float distance = Vector3.Distance(gameObject.transform.position, collider.gameObject.transform.position);
+                    if (distance < nearestDistance)
+                    {
+                        nearestDistance = distance;
+                        nearestCustomer = collider.gameObject;
+                    }
+
+                    if (nearestCustomer != null)
+                    {
+                        nextIndicator.transform.position = nearestCustomer.transform.position + new Vector3(0, 2f, 0);
+                    }
+
+                    if (Input.GetKeyDown("space"))
+                    {
+                        StartCoroutine(SwitchCustomer(nearestCustomer));
+                    }
+
                     break; 
                 }
             }
