585725924382b73979d16922afd7fdbf5777746c
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-03-22 22:46:31 +0100

    Add spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float SpawnInterval = 5f;
+    public List<GameObject> CustomerPrefabs;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnCustomer", 1f, 1f);
+    }
+
+    void SpawnCustomer()
+    {
+        if (CustomerPrefabs.Count > 0) {
+            Random random = new Random();
+            Instantiate(CustomerPrefabs[(int)Mathf.Round(Random.Range(0, CustomerPrefabs.Count))], gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
