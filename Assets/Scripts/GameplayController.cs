39c267b5a837720e49e274b5d536b95d5275e020
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-03-22 23:11:34 +0100

    Add UI

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayController : MonoBehaviour
 {
-    
+    private int score = 0;
+    public Text scoreText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public void AddScore()
+    {
+        score++;
     }
 }
